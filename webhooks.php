0171d868bb96a0531170e1fe8995396b506007d8
chatiker <87030077+chatiker@users.noreply.github.com>
2021-07-06 23:51:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'a/KSAhXCP4p5RT4p29SiFzlBrWWPNEUWaIfuZtzt/6xhbvQD527V+BemmfFnTXJqLtnA+MDbxVCpRyg5nQlVvMJGkX8TNPa/UfLFxlcnk0LzZip0ze77ML4bQu1L+XnPem8KUSxupCtugEwonrBCewdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
